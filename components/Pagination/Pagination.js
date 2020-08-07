a7c4a460aeb931b11cd89656126ae172021ceba3
Itzel Enciso <enciso.iq@gmail.com>
2020-08-06 21:01:36 -0500

    Fix pagination

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getComic } from 'actions';
-import { getCurrentComic } from 'selectors';
+import { getCurrentComic, getTotal } from 'selectors';
 
 const Pagination = ({ next_text, previous_text }) => {
   const dispatch = useDispatch();
+  const total = useSelector(getTotal);
   const currentComic = useSelector(getCurrentComic);
 
   const handleClick = e => {
@@ -17,22 +18,28 @@ const Pagination = ({ next_text, previous_text }) => {
   return (
     <nav className='pagination column is-rounded' role='navigation' aria-label='pagination'>
       <button
+        title='next'
+        onClick={e => handleClick(e)}
+        className='pagination-next button is-dark'
+        disabled={currentComic === total}
+      >
+        {next_text}
+      </button>
+      <button
         title='prev'
         onClick={e => handleClick(e)}
         className='pagination-previous button is-dark'
+        disabled={currentComic === 1}
       >
         {previous_text}
-      </button>
-      <button title='next' onClick={e => handleClick(e)} className='pagination-next button is-dark'>
-        {next_text}
       </button>
     </nav>
   );
 };
 
 Pagination.defaultProps = {
-  next_text: 'Anterior',
-  previous_text: 'Siguiente',
+  next_text: 'Siguiente',
+  previous_text: 'Anterior',
 };
 
 Pagination.propTypes = {
